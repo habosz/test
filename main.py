f8e7faae616b8823681fd4c6c1bcad8001d7fea2
Hanna Bogusz <hanbogusz@192.168.1.3>
2024-11-16 15:02:30 +0100

    dodanie main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+# user_age = input("Ile masz lat:")
+# print(user_age)
+
+# a = 1,546677
+# b = 3
+# b = 3
+# b = 3
+
+# print('''
+#       pierwsza linijka 
+#         druga linijka
+# ''')
+
+
+# a = 10
+# b = 3
+
+# import math
+
+# print(f'Dodawanie: {a+b}')
+# print(f'Odejmowanie: {a-b}')
+# print(f'Mnozenie: {a*b}')
+# print(f'Dzielenie: {a/b}')
+# print(f'Moduł: {a%b}') #zwraca resztę z dzielenia
+# print(f'Potęgowanie: {a**b}')
+# print(f'Dzielenie całkowite: {a//b}')
+# print(f'pierwiastkowanie: {math.sqrt{a}}')
+
+
+# i = 5
+# while i <= 5:
+#     print(i)
+#     i += 1
+# print(f'{i}')
+# print(i)
+
+# i = 6
+# for x in range(6, 1, -1):
+#     print(x)
+
+
+# lista = [10, 20, 30, 40, 5, 9]
+# print(lista[0])
+# print(lista[2:4])
+
+
+# # stworzenie listy za pomocą range
+# MyList = [9, 8, 6, 4, 3, 5]
+
+# for i in range(len(MyList)):
+#     print(i, MyList(i))
+
+# # for i in MyList:
+
+
+# original = " Python is COOL!"
+# # lower_cased = original.lower()
+# # print(lower_cased)
+
+# stripped = original.strip()
+# print(stripped)
+
+# splitted = original.split()
+# print(splitted)
+
+# assert stripped == "Python is COOL!"
+
+# ugly = ' tiTle of mY nEw BoOk'
+# pretty = ugly.title().strip()
+
+# if pretty is "Title of My New Book" is True:
+#     print("OK")
+
+
+a = 2
+b = 3
+c = 2
+
+result = 6*a**3 - ((8*(b**2))/(4*c)) + 11
+
+assert result == 50
